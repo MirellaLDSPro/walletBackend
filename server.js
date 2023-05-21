79fde03a68384efdd13dc4b76056fa8623a2739c
Mirella Lins <mirella.lds@gmail.com>
2023-05-20 23:30:29 -0300

    update

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
+// Add this before server.use(router)
 server.use(
-  jsonServer.rewriter({
-   "/api/*": "/$1",
-  })
- );
-server.use(router)
+	// Add custom route here if needed
+	jsonServer.rewriter({
+		"/api/*": "/$1",
+	})
+);
+server.use(router);
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+	console.log("JSON Server is running");
+});
 
-module.exports = server
+// Export the Server API
+module.exports = server;
