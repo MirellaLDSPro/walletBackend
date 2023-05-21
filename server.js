fe7b18e98a525360c2fe5ad3fc0c0975bde1914d
Mirella Lins <mirella.lds@gmail.com>
2023-05-21 15:47:06 -0300

    test

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now()
+    console.log("Mirella Lins")
   }
   // Continue to JSON Server router
   next()
