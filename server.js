bf19f031d938b56d6cee75c42bfd7fdf59463cf8
Mirella Lins <mirella.lds@gmail.com>
2023-05-21 04:33:32 -0300

    Requests post

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ server.use(
 		"/api/*": "/$1",
 	})
 );
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
 server.use(router);
 server.listen(3000, () => {
 	console.log("JSON Server is running");
